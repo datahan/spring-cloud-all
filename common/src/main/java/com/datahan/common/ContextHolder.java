0ae6db5fb970c73c0bba03e03e1c0c98cd62c149
datahan <datawisher@gmail.com>
2019-06-22 19:52:59 +0800

    加入ContextHolder

---
+++ common/src/main/java/com/datahan/common/ContextHolder.java
@@ -0,0 +1,125 @@
+package com.datahan.common;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.Serializable;
+
+/**
+ * 项目上下文
+ * @author Jim
+ */
+@Component
+public class ContextHolder implements ApplicationContextAware {
+
+  private static ApplicationContext applicationContext;
+
+
+  /**
+   * 获取当前线程的request
+   */
+  public static HttpServletRequest getRequest() {
+    return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+  }
+
+  /**
+   * 获取当前线程的response
+   */
+  public static HttpServletResponse getResponse() {
+    return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
+  }
+
+  /**
+   * 获取HttpSession
+   */
+  public static HttpSession getHttpSession() {
+    return getRequest().getSession();
+  }
+
+  /**
+   * 把值放入session
+   */
+  public static void setSessionAttribute(String key, Serializable entity) {
+    getHttpSession().setAttribute(key, entity);
+  }
+
+  public static Object getSessionAttribute(String key) {
+    return getHttpSession().getAttribute(key);
+  }
+
+  /**
+   * 把值放入当前request
+   */
+  public static void setRequestAttribute(String key, Object entity) {
+    getRequest().setAttribute(key, entity);
+  }
+
+  public static Object getRequestAttribute(String key) {
+    return getRequest().getAttribute(key);
+  }
+
+  @Override
+  public void setApplicationContext(ApplicationContext context) {
+    Assert.notNull(context, "WebApplicationContext can not be null");
+    applicationContext = context;
+  }
+
+  public static ApplicationContext getApplicationContext() {
+    return applicationContext;
+  }
+
+  /**
+   * 根据beanId获取bean
+   */
+  @SuppressWarnings("unchecked")
+  public static <T> T getBean(String beanId) {
+    return (T) applicationContext.getBean(beanId);
+  }
+
+//  /**
+//   * 获取当前登录用户
+//   */
+//  public static User getLoginUser() {
+//    SecurityContext ctx = SecurityContextHolder.getContext();
+//    Authentication auth = ctx.getAuthentication();
+//    if (auth != null && auth.getPrincipal() instanceof User) {
+//      return (User) auth.getPrincipal();
+//    }
+//    return null;
+//  }
+//
+//  /**
+//   * 获取当前登录用户 id
+//   */
+//  public static Long getLoginUserId() {
+//    User user = getLoginUser();
+//    return user == null ? null : user.getId();
+//  }
+//
+//  /**
+//   * 获取当前登录用户,获取为空抛出异常
+//   */
+//  public static User getRequiredLoginUser() {
+//    User user = getLoginUser();
+//    if (user == null) {
+//      throw new NoneLoginException("用户未登录");
+//    }
+//    return user;
+//  }
+//
+//  /**
+//   * 获取当前登录用户 id，获取为空抛出异常
+//   */
+//  public static Long getRequiredLoginUserId() {
+//    return getRequiredLoginUser().getId();
+//  }
+
+}
--- server-amager/src/main/java/com/datahan/server/amager/controller/AmagerController.java
+++ server-amager/src/main/java/com/datahan/server/amager/controller/AmagerController.java
@@ -1,9 +1,9 @@
 package com.datahan.server.amager.controller;
 
+import com.datahan.common.ContextHolder;
 import com.datahan.log.LogAspect;
 import com.datahan.server.amager.domain.Greeting;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,16 +18,13 @@ import java.util.concurrent.atomic.AtomicLong;
 @Slf4j
 public class AmagerController {
 
-    @Autowired
-    private HttpServletRequest request;
-
     private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     @LogAspect("执行greeting方法")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        //HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        HttpServletRequest request = ContextHolder.getRequest();
         String hobby = request.getHeader("Hobby");
         log.info("hobby is a " + hobby);
         return new Greeting(counter.incrementAndGet(),
