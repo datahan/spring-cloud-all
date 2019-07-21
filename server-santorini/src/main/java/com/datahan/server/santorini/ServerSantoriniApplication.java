95398b0a563650959b3d5e8795941e2e97b21ad8
datahan <datawisher@gmail.com>
2019-07-21 15:20:03 +0800

    auto configuration

--- server-santorini/src/main/java/com/datahan/server/santorini/ServerSantoriniApplication.java
+++ server-santorini/src/main/java/com/datahan/server/santorini/ServerSantoriniApplication.java
@@ -1,10 +1,23 @@
 package com.datahan.server.santorini;
 
+import com.datahan.server.amager.autoconfig.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ServerSantoriniApplication {
+
+    @Autowired
+    private HelloService helloService;
+
+    @RequestMapping("/")
+    public String index() {
+        return helloService.sayHello();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServerSantoriniApplication.class, args);
