be61307f569df04bbdbfc5a1757d7ca5a774debc
jing.han <jing.han@ezhiyang.com>
2019-07-23 09:44:25 +0800

    update project

--- api-gateway/src/main/java/com/datahan/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/datahan/apigateway/ApiGatewayApplication.java
@@ -2,11 +2,13 @@ package com.datahan.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * @author Jim
+ * @author Jim Han
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
--- zuul-gateway/src/main/java/com/datahan/demo/ZuulApplication.java
+++ zuul-gateway/src/main/java/com/datahan/demo/ZuulApplication.java
@@ -2,12 +2,14 @@ package com.datahan.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableEurekaClient
+public class ZuulApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(ZuulApplication.class, args);
     }
 
 }
